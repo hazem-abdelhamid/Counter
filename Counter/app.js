c8093289e483293de7520362c258e0688704c23e
hazem-abdelhamid <143886892+hazem-abdelhamid@users.noreply.github.com>
2023-11-10 15:21:03 +0200

    Add files via upload

---
+++ Counter/app.js
@@ -0,0 +1,28 @@
+let count = 0;
+
+const btns = document.querySelectorAll(".btn");
+
+const value = document.querySelector("#value");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#102a42";
+    }
+    value.textContent = count;
+  });
+});
